88e13b3414df4f41dbbcbfc8e7ebcbd7e98450ad
snowman <sreevardhan73@gmail.com>
2021-05-18 02:15:20 +0530

    First commit

---
+++ src/main/java/integrator/integrator.java
@@ -0,0 +1,16 @@
+package integrator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class integrator {
+    public static final Logger logger = LogManager.getLogger(integrator.class);
+
+    public integrator() {}
+
+    public static void main(String[] args){}
+
+
+
+
+}
